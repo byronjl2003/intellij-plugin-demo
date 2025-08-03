8b88b111955e9c3148fc6fc1497df5aa67e85c10
GitHub Action <action@github.com>
2025-08-03 07:53:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/byronjl2003/intellijplugindemo/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/byronjl2003/intellijplugindemo/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.byronjl2003.intellijplugindemo.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
